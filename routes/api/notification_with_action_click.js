603758af6a83842f396643e6d0982b5a4fab9ae3
nithahuwaida <hafizhahuwaida@gmail.com>
2020-12-23 09:39:38 +0700

    notification with action click

---
+++ routes/api/notification_with_action_click.js
@@ -0,0 +1,49 @@
+const express = require('express');
+var admin = require("firebase-admin");
+var serviceAccount = require("../../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const router = express.Router();
+
+
+router.post('/topic', (req,res)=>{
+    var topicName = 'industry-tech'
+
+    var message = {
+    notification: {
+        title: 'Breaking News....'
+    },
+    android: {
+        notification: {
+        click_action: 'https://www.google.com/'
+        }
+    },
+    apns: {
+        payload: {
+        aps: {
+            'category': 'INVITE_CATEGORY'
+        }
+        }
+    },
+    webpush: {
+        fcm_options: {
+        link: 'https://www.google.com/'
+        }
+    },
+    topic: topicName,
+    };
+
+    admin.messaging().send(message)
+    .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+        console.log('Error sending message:', error);
+    });
+});
+
+module.exports = router;
