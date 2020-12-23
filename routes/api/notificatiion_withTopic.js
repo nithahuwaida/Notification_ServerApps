4124beca618ea2604b30cac698c77a2a2f19412e
nithahuwaida <hafizhahuwaida@gmail.com>
2020-12-23 09:37:12 +0700

    notification with metode topic

---
+++ routes/api/notificatiion_withTopic.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fetch = require('node-fetch');
+var dotenv = require('dotenv');
+dotenv.config();
+
+const router = express.Router();
+
+router.post('/sendToAll', (req,res)=>{
+    var notification ={
+        'title' : 'Title of notification',
+        'text' : 'Subtitle'
+    };
+
+    var notification_body = {
+        //notification with topic
+        'to' : '/topics/topic',
+        'notification' : notification,
+    }
+
+    fetch('https://fcm.googleapis.com/fcm/send', {
+        'method' : 'POST',
+        'headers' : {
+            'Authorization' : 'key='+`${process.env.FIREBASE_KEY}`,
+            'Content-Type' : 'application/json'
+        },
+        'body' : JSON.stringify(notification_body)
+    }).then((response)=>{
+        console.log(response)
+        res.status(200).send('Notification send successfully');
+    }).catch((err)=>{
+        res.status(400).send('Something went wrong!');
+        console.log(err);
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/api/notification/devices',require('./routes/api/notification_withIdDevices'));
+app.use('/api/notification/topic',require('./routes/api/notification_withTopic'));
 
 const PORT = process.env.PORT || 5123 ;
 
