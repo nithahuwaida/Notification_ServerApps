64e774613c677c41cb15d9fdbf5c7ef20b05ed5a
nithahuwaida <hafizhahuwaida@gmail.com>
2021-01-02 01:39:56 +0700

    update configuration firebase-admin + serviceAccountKey

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
 const app = express();
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use('/api/notification/devices',require('./routes/api/notification_withIdDevices'));
 app.use('/api/notification/topic',require('./routes/api/notification_withTopic'));
-app.use('/api/notification/topic/action',require('./routes/api/notification_with_action_click'));
+app.use('/api/notification/topic/action',require('./routes/api/notification_with_action_click_v2'));
 
 const PORT = process.env.PORT || 5123 ;
 
